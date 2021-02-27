862b491fde3c96fa88881f54dcd363d6826b75a2
CasaRol <casarol@PC1681.nordic.local>
2021-02-27 17:00:14 +0100

    Feature: Started working on question template in questionChild component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div id="app">
     <h1 id="pageTitle">Trivia Game!</h1>
-    <intro v-if="showIntro" :/>
-    <question-main v-if="showQuestionMain"/>
+    <question-main />
   </div>
 </template>
 
 <script>
-import Intro from "./components/Intro.vue";
-import QuestionMain from './components/QuestionMain.vue';
-
+import QuestionMain from "./components/QuestionMain.vue";
 
 export default {
   name: "App",
   data() {
     return {
       showIntro: true,
-      showQuestionMain: false
-    }
+      showQuestionMain: false,
+    };
   },
   components: {
-    Intro,
     QuestionMain,
   },
 };
--- src/components/QuestionChild.vue
+++ src/components/QuestionChild.vue
@@ -1,13 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <h1>{{ question.mainQuestion }}</h1>
+    <button v-for="option in question.options" :key="option">{{ option }}</button>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["question"],
+};
 </script>
 
 <style>
-
 </style>
